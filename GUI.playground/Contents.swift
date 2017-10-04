c49d22353a554b113403ed3a5a123fb9c62a50b6
Heaps <dhea8398@740s-pl-114.local>
2017-10-04 08:49:17 -0600

    Messed around with views and made some labels.

--- GUI.playground/Contents.swift
+++ GUI.playground/Contents.swift
@@ -11,3 +11,15 @@ var temp = firstButton.titleLabel
 var secondButton = UIButton(frame: CGRect(x: 50, y: 50, width: 200, height: 120))
 secondButton.backgroundColor = .black
 secondButton.setTitle("BUTTON DEUX", for: UIControlState.normal)
+var testView = UIView(frame: CGRect(x: 50, y: 50, width: 400, height: 300))
+var firstLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+firstLabel.center = CGPoint(x: 160, y: 285)
+firstLabel.textAlignment = .center
+firstLabel.text = "Sweeto Burrito"
+testView.addSubview(firstLabel)
+
+var secondLabel = UILabel(frame: CGRect(x: 50, y: 50, width: 200, height: 21))
+secondLabel.center = CGPoint(x: 160, y: 285)
+secondLabel.textAlignment = .left
+secondLabel.text = "I am not right"
+
